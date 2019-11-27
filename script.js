a25ef6835a9d02351d58ff771a083551f459517b
Teresa <teresacz@op.pl>
2019-11-27 20:03:34 +0100

    Make hamburger menu disappear on click anywhere beside menu

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var burger = document.querySelector(".hamburger");
-burger.addEventListener("click", function(){
+const burger = document.querySelector(".hamburger");
+
+burger.addEventListener("click", function () {
   burger.classList.toggle("change");
+});
+
+document.body.addEventListener("click", function (event) {
+  if (!event.target.matches(".hamburger, .bar, .list li")) {
+    if (burger.classList.contains("change")) {
+      burger.classList.remove("change");
+    }
+  }
 });
